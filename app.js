ae4d6ab10049381213b5f1e9261b4c23998f2d1c
AbdelrhmanAmin <maxhd213@gmail.com>
2021-09-01 20:59:57 +0200

    fix eslint by moving my code into load event and assign functions to the elements

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const handleMenuBar = () => {
-  document.getElementById('mobile-nav').style.display = 'flex';
-  const elementsToHide = Array.from(document.querySelectorAll('.hide-nav-menu-mobile'))
-  elementsToHide.forEach((x) => x.style.display = 'none')
-}
-const handleReverse = () => {
-  document.getElementById('mobile-nav').style.display = 'none';
-  const elementsToHide = Array.from(document.querySelectorAll('.hide-nav-menu-mobile'))
-  elementsToHide.forEach((x) => x.style.display = 'flex')
-}
+
+window.addEventListener('load', () => {
+  const handleMenuBar = () => {
+    document.getElementById('mobile-nav').style.display = 'flex';
+    const elementsToHide = Array.from(document.querySelectorAll('.hide-nav-menu-mobile'));
+    elementsToHide.forEach((x) => x.style.display = 'none');
+    return
+  };
+
+  const handleReverse = () => {
+    document.getElementById('mobile-nav').style.display = 'none';
+    const elementsToHide = Array.from(document.querySelectorAll('.hide-nav-menu-mobile'));
+    elementsToHide.forEach((x) => x.style.display = 'flex');
+    return
+  };
+
+  document.getElementById('menu-btn').addEventListener('click', handleMenuBar);
+  document.getElementById('nav-btn').addEventListener('click', handleReverse);
+  document.getElementById('exit-btn').addEventListener('click', handleReverse);
+  return
+})
